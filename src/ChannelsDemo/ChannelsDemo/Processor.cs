1d453770225ee35a118ee5910b40ec5d6fc6d1cb
Chavp <my.parinya@gmail.com>
2025-08-13 20:37:24 +0700

    add Channels

---
+++ src/ChannelsDemo/ChannelsDemo/Processor.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Threading.Channels;
+
+namespace ChannelsDemo
+{
+    public class Processor : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private readonly Channel<ChannelRequest> _channel;
+
+        public Processor(ILogger<Processor> logger,
+            Channel<ChannelRequest> channel)
+        {
+            _logger = logger;
+            _channel = channel;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //while (!stoppingToken.IsCancellationRequested)
+            //{
+            //    //await Task.Delay(1000);
+            //    //_logger.LogInformation(DateTime.Now.ToString());
+            //}
+            while (await _channel.Reader.WaitToReadAsync(stoppingToken))
+            {
+                var request = await _channel.Reader.ReadAsync(stoppingToken);
+                _logger.LogInformation(request.Message);
+            }
+        }
+    }
+
+    public record ChannelRequest(string Message);
+}
---
+++ src/ChannelsDemo/ChannelsDemo/Program.cs
@@ -0,0 +1,22 @@
+using ChannelsDemo;
+using System.Threading.Channels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHostedService<Processor>();
+
+builder.Services.AddSingleton<Channel<ChannelRequest>>(_ => Channel.CreateUnbounded<ChannelRequest>(new UnboundedChannelOptions
+{
+    SingleReader = true,
+    AllowSynchronousContinuations = false,
+}));
+
+var app = builder.Build();
+
+app.Map("send", async (Channel<ChannelRequest> channel) =>
+{
+    await channel.Writer.WriteAsync(new ChannelRequest($"Hello from {DateTime.UtcNow}"));
+    return Results.Ok();
+});
+
+app.Run();
